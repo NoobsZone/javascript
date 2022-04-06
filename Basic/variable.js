c5c0e1003d53b0e208a073d7ff0666534809ca52
Anoy <anoy.eatl@gmail.com>
2022-04-07 00:00:22 +0600

    variables

---
+++ Basic/variable.js
@@ -0,0 +1,28 @@
+//Variable is like a box where we store values.
+// let a = 1000000;
+// let array = [10,20,30,40,50,60]
+// console.log('a', a)
+// console.log('array', array)
+
+let asif1 ="Moyna" // Changable
+asif1 = "Goyna"
+var asif2 = "Moyna" // changable
+asif2 = "Goyna"
+const asif3 = "Moyna"
+
+const pi = 3.1416;// Unchancgable
+console.log('pi', pi)
+// asif3="Goyna"
+let gugu = "huhu"
+function lulu(){
+    let gugu = "huhu"
+    var pupu = "kuku"
+    console.log('pupu', pupu);
+}
+lulu()
+
+console.log(gugu)
+
+console.log('asif1', asif1);
+console.log('asif2', asif2);
+console.log('asif3', asif3);
