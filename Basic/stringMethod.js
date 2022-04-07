26a5374ef7c9178191d1fd6fb9951a30cacff777
Anoy <anoy.eatl@gmail.com>
2022-04-07 23:26:36 +0600

    slice

--- Basic/stringMethod.js
+++ Basic/stringMethod.js
@@ -2,3 +2,9 @@ let numb = 5;
 let text = numb.toString();
 // let padded = text.padStart(4,"0");
 console.log(typeof text);
+
+//slice
+
+let str = "Apple, Banana, Kiwi";
+let sliceMethod = str.slice(7,13);
+console.log(sliceMethod);
