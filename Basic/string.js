f01a342d30b2b1e9b0edba86b238901a7d7dc705
Anoy <anoy.eatl@gmail.com>
2022-04-07 23:10:09 +0600

    string methods

---
+++ Basic/string.js
@@ -0,0 +1,9 @@
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let gugu = text.includes("E",4);
+let huhu = text.replace("E","K")
+let lalu = text.indexOf("A",1);
+// let nunu = text.
+console.log('lalu', lalu)
+console.log('gugu', gugu)
+let length = text.length;
+console.log('length', length)
