0aebb015b5db9f3a4e9588ba910cc5001b9ffa42
Anoy <anoy.eatl@gmail.com>
2022-04-10 01:11:36 +0600

    syllabus

---
+++ Basic/Loop.js
@@ -0,0 +1,21 @@
+/*   
+
+1. let,const,var
+2. Operators //
+
+== normal equal
+=== type checking
++= x=x+y
+&& Logical AND
+|| Logical OR
+!=
+
+3.Object and this keyword
+4.Function
+5.Array
+6.Conditional Statement 
+if-else
+swithc-case
+
+
+*/ 
